38551d03639bf7e06365ab73b2aab2a19a1c73bb
Aqra Irawan <akirawan@stu.naperville203.org>
2026-01-19 15:54:06 -0600

    fixed some stuff and got done with all the fuelIntake files (locked in hard today)

--- src/main/java/frc/robot/subsystems/fuelIntake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/fuelIntake/IntakeConstants.java
@@ -3,48 +3,31 @@ package frc.robot.subsystems.fuelIntake;
 import edu.wpi.first.units.measure.Distance;
 
 public class IntakeConstants {
+
+  private static final String CONSTRUCTOR_EXCEPTION = "constant class";
+
+  private IntakeConstants() {
+    throw new IllegalStateException(CONSTRUCTOR_EXCEPTION);
+  }
+
     public static final double DEPLOYER_KP = 0;
-
-
     public static final double DEPLOYER_KI = 0;
-
-
     public static final double DEPLOYER_KD = 0;
-
-
     public static final double DEPLOYER_KS = 0;
-
-
     public static final double DEPLOYER_KV = 0;
-
-
     public static final double DEPLOYER_KA = 0;
-
-
     public static final double DEPLOYER_KG = 0;
-
-
     public static final double DEPLOYER_KV_EXPO = 0;
-
-
     public static final double DEPLOYER_KA_EXPO = 0;
 
 
     public static final int ROLLER_MOTOR_ID = 0;
-
-
     public static final int DEPLOYER_MOTOR_ID = 0;
 
 
     public static final Distance DEPLOYER_CIRCUIMFERENCE = null;
-
-
     public static final double DEPLOYER_CRUISE_VELOCITY = 0;
-
-
     public static final double DEPLOYER_PEAK_CURRENT_LIMIT = 0;
-
-
     public static final double DEPLOYER_LOWER_CURRENT_LIMIT = 0;
 
 
@@ -52,17 +35,9 @@ public class IntakeConstants {
 
 
     public static final double ROLLER_PEAK_CURRENT_LIMIT = 0;
-
-
     public static final double ROLLER_LOWER_CURRENT_LIMIT = 0;
-
-
     public static final double INTAKE_ROLLER_VELOCITY_RPS = 0;
-
-
     public static final Distance DEPLOYED_POSITION_ROTATIONS = null;
-
-
     public static final Distance RETRACTED_POSITION_ROTATIONS = null;
 
    
