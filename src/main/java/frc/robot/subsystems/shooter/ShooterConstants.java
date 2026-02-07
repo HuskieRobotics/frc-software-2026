037b0638985fd5f2d4ad5a16ef3e99d65201afdf
Fatema Merchant <fnmerchant@stu.naperville203.org>
2026-02-07 12:10:26 -0600

    configure position requests for hood and turret correctly

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -18,7 +18,7 @@ public class ShooterConstants {
   // change the ids once we get the updated stuff
   public static final int FLYWHEEL_LEAD_MOTOR_ID = 9; // Updated in robot devices and trackers sheet
   public static final int FLYWHEEL_FOLLOW_1_MOTOR_ID = 21;
-  ; // FIXME: update from robot devices and trackers sheet
+  // FIXME: update from robot devices and trackers sheet
   public static final int FLYWHEEL_FOLLOW_2_MOTOR_ID = 22;
   public static final int HOOD_MOTOR_ID = 23;
   public static final int TURRET_MOTOR_ID = 14;
