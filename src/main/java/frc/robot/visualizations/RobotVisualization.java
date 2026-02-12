cc82d89ffee2d5534355ed71d942f207a14b860d
Kush Shah <kmshah2@stu.naperville203.org>
2026-02-12 09:38:21 -0600

    rename to intake

--- src/main/java/frc/robot/visualizations/RobotVisualization.java
+++ src/main/java/frc/robot/visualizations/RobotVisualization.java
@@ -8,7 +8,8 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.lib.team3061.RobotConfig;
 import frc.lib.team3061.leds.LEDs;
-import frc.robot.subsystems.fuelIntake.Intake;
+import frc.robot.subsystems.intake.Intake;
+
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
 import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
