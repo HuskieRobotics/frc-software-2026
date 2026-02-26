98767652fb6d71480d204575461ed834e300da1e
Fatema Merchant <fnmerchant@stu.naperville203.org>
2026-02-25 21:02:53 -0600

    rename game piece to fuel

--- src/main/java/frc/lib/team3061/leds/LEDs.java
+++ src/main/java/frc/lib/team3061/leds/LEDs.java
@@ -72,9 +72,9 @@ public abstract class LEDs extends SubsystemBase {
     UNTILTING_ROBOT((leds, section) -> leds.strobe(section, Color.kRed, STROBE_SLOW_DURATION)),
     DRIVE_TO_POSE_CANCELED(
         (leds, section) -> leds.strobe(section, Color.kPink, STROBE_SLOW_DURATION)),
-    EJECTING_GAME_PIECE(
+    EJECTING_FUEL(
         (leds, section) -> leds.strobe(section, new Color(255, 20, 0), STROBE_SLOW_DURATION)),
-    RELEASING_GAME_PIECE(
+    RELEASING_FUEL(
         (leds, section) -> leds.strobe(section, Color.kGreen, STROBE_SLOW_DURATION)),
     AUTO_DRIVING_TO_POSE((leds, section) -> leds.orangePulse(section, PULSE_DURATION)),
     AT_POSE((leds, section) -> leds.solid(section, Color.kGreen)),
