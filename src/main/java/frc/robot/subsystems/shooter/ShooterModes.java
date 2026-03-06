6b9c22a461b74ed942d3da297445e52a01cebbf8
Geoff Schmit <gcschmit@gmail.com>
2026-03-06 13:27:33 -0600

    Remove center camera

--- src/main/java/frc/robot/subsystems/shooter/ShooterModes.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterModes.java
@@ -43,7 +43,7 @@ public class ShooterModes extends SubsystemBase {
   private final Shooter shooter;
 
   private boolean hubActive;
-  private double shotVelocityMultiplier = 0.98;
+  private double shotVelocityMultiplier = 0.97;
 
   /*
   Create interpolating tree map for data points
