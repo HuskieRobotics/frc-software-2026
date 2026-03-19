95bbd0b4d7bea6cef81bae34a0d0e0842fd9ad14
Geoff Schmit <gcschmit@gmail.com>
2026-03-19 13:14:54 -0500

    Add two new cameras to competition bot.

--- src/main/java/frc/robot/configs/New2026RobotConfig.java
+++ src/main/java/frc/robot/configs/New2026RobotConfig.java
@@ -115,6 +115,8 @@ public class New2026RobotConfig extends RobotConfig {
 
   private static final String BR_CAMERA_SERIAL_NUMBER = "40708542";
   private static final String BL_CAMERA_SERIAL_NUMBER = "40708556";
+  private static final String BCL_CAMERA_SERIAL_NUMBER = "40777404";
+  private static final String BCR_CAMERA_SERIAL_NUMBER = "40777399";
 
   private static final int MONO_EXPOSURE = 2200;
   private static final double MONO_GAIN = 15;
@@ -142,6 +144,26 @@ public class New2026RobotConfig extends RobotConfig {
               Units.inchesToMeters(11.729),
               Units.inchesToMeters(7.434)),
           new Rotation3d(0, Units.degreesToRadians(-25), Units.degreesToRadians(90.0)));
+
+  // Back center left camera
+  // x, y, z, pitch, yaw
+  private static final Transform3d ROBOT_TO_BCL_CAMERA =
+      new Transform3d(
+          new Translation3d(
+              Units.inchesToMeters(-11.5),
+              Units.inchesToMeters(6.1725),
+              Units.inchesToMeters(11.569)),
+          new Rotation3d(0, Units.degreesToRadians(-10.00), Units.degreesToRadians(160.0)));
+
+  // Back center right camera
+  // x, y, z, pitch, yaw
+  private static final Transform3d ROBOT_TO_BCR_CAMERA =
+      new Transform3d(
+          new Translation3d(
+              Units.inchesToMeters(-11.5),
+              Units.inchesToMeters(4.0415),
+              Units.inchesToMeters(11.569)),
+          new Rotation3d(0, Units.degreesToRadians(-10.00), Units.degreesToRadians(200.0)));
 
   // use AprilTag ID 13 for empirical determination of the robot-to-camera transform
   private static final Pose3d ROBOT_TO_TAG_13_BACK_CAMERAS =
@@ -188,7 +210,7 @@ public class New2026RobotConfig extends RobotConfig {
           .poseForRobotToCameraTransformCalibration(ROBOT_TO_TAG_13_RIGHT_CAMERA)
           .id(BR_CAMERA_SERIAL_NUMBER)
           .location("BR")
-          .width(1800)
+          .width(1600)
           .height(1200)
           .exposure(MONO_EXPOSURE)
           .gain(MONO_GAIN)
@@ -200,7 +222,31 @@ public class New2026RobotConfig extends RobotConfig {
           .poseForRobotToCameraTransformCalibration(ROBOT_TO_TAG_13_LEFT_CAMERA)
           .id(BL_CAMERA_SERIAL_NUMBER)
           .location("BL")
-          .width(1800)
+          .width(1600)
+          .height(1200)
+          .exposure(MONO_EXPOSURE)
+          .gain(MONO_GAIN)
+          .denoise(MONO_DENOISE)
+          .stdDevFactor(1.0)
+          .build(),
+      CameraConfig.builder()
+          .robotToCameraTransform(ROBOT_TO_BCL_CAMERA)
+          .poseForRobotToCameraTransformCalibration(ROBOT_TO_TAG_13_BACK_CAMERAS)
+          .id(BCL_CAMERA_SERIAL_NUMBER)
+          .location("BCL")
+          .width(1600)
+          .height(1200)
+          .exposure(MONO_EXPOSURE)
+          .gain(MONO_GAIN)
+          .denoise(MONO_DENOISE)
+          .stdDevFactor(1.0)
+          .build(),
+      CameraConfig.builder()
+          .robotToCameraTransform(ROBOT_TO_BCR_CAMERA)
+          .poseForRobotToCameraTransformCalibration(ROBOT_TO_TAG_13_BACK_CAMERAS)
+          .id(BCR_CAMERA_SERIAL_NUMBER)
+          .location("BCR")
+          .width(1600)
           .height(1200)
           .exposure(MONO_EXPOSURE)
           .gain(MONO_GAIN)
