8880fc9618711d4df14eb7da01e65ff021bf71db
Kush Shah <kmshah2@stu.naperville203.org>
2026-03-27 11:02:05 -0500

    Update at 'Fri Mar 27 11:02:05 CDT 2026'

--- src/main/java/frc/robot/subsystems/shooter/ShooterModes.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterModes.java
@@ -47,7 +47,7 @@ public class ShooterModes extends SubsystemBase {
   private Timer shiftTimer = new Timer();
   private double shiftTimerOffset = 0.0;
   private boolean hubActive;
-  private double shotVelocityMultiplier = 0.97;
+  private double shotVelocityMultiplier = 0.98;
   private double turretAngleAdjustmentDeg = 0.0;
 
   private boolean shootOnTheMoveInAuto = false;
