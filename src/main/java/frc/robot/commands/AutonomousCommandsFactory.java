5df9ac91eb639318d4cd450b90a4770f6216c5e3
Stefan Ilic <stefan.ilic314@gmail.com>
2026-04-13 20:39:17 -0500

    add support autos to chooser

--- src/main/java/frc/robot/commands/AutonomousCommandsFactory.java
+++ src/main/java/frc/robot/commands/AutonomousCommandsFactory.java
@@ -106,6 +106,24 @@ public class AutonomousCommandsFactory {
         rightNeutralZoneSweepAndOutpost(drivetrain, hopper, intake, shooter, shooterModes));
 
     autoChooser.addOption(
+        "Wide Left Far Hub Support", getLeftFarHubWide(drivetrain, hopper, intake, shooter));
+
+    autoChooser.addOption(
+        "Narrow Left Far Hub Support", getLeftFarHubNarrow(drivetrain, hopper, intake, shooter));
+
+    autoChooser.addOption(
+        "Wide Right Far Hub Support", getRightFarHubWide(drivetrain, hopper, intake, shooter));
+
+    autoChooser.addOption(
+        "Narrow Right Far Hub Support", getRightFarHubNarrow(drivetrain, hopper, intake, shooter));
+
+    autoChooser.addOption(
+        "Left Close Hub Bait Support", getLeftCloseHubBait(drivetrain, hopper, intake, shooter));
+
+    autoChooser.addOption(
+        "Left Mid Support", getLeftMidSupport(drivetrain, hopper, intake, shooter));
+
+    autoChooser.addOption(
         "Outpost and Depot", outpostAndDepot(drivetrain, hopper, intake, shooter, shooterModes));
 
     /************ Start Point ************
